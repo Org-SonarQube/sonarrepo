b412d34b36ce2272a833ac6edef9cca4e6ba00fd
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-21 19:41:43 +0530

    Update safe_templates.js

--- tests/assets/node_source/true_negatives/safe_templates.js
+++ tests/assets/node_source/true_negatives/safe_templates.js
@@ -1,4 +1,4 @@
-function name() {
+/* function name() {
     var x = '<h1>hell0</h1>'
     var y = new Handlebars.escapeExpression(x);
     return new Handlebars.escapeExpression('<img src="" onload=alert(0)>');
@@ -17,4 +17,4 @@ var template = "This is {{target}}";
 var target = "user's pictures";
 var result = Handlerbars.compile(template, { noEscape: false })({ target: target });
 
-Sqrl.autoEscaping(true)
+Sqrl.autoEscaping(true) */
