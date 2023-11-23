5d9497c6013309f23ecc8a9df9c5c45771df77aa
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-23 17:42:46 +0530

    Update safe_shelljs.js

--- tests/assets/node_source/true_negatives/safe_shelljs.js
+++ tests/assets/node_source/true_negatives/safe_shelljs.js
@@ -1,12 +1,13 @@
 const shell = require('shelljs');
 const express = require('express')
 const router = express.Router()
-
+/*
 router.get('/greeting', (req, res) => {
     // ok
     const input = 'ls ./'
     return shell.exec(input, { silent: true })
 })
+*/
 
 router.get('/foo', (req, res) => {
     // ok
