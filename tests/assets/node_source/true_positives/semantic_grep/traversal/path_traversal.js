ec23e923231f66723ae87baa7e9e34c3d34d319b
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-23 17:32:06 +0530

    Update path_traversal.js

--- tests/assets/node_source/true_positives/semantic_grep/traversal/path_traversal.js
+++ tests/assets/node_source/true_positives/semantic_grep/traversal/path_traversal.js
@@ -8,21 +8,7 @@ var Promise = require('bluebird');
 Promise.promisifyAll(fileSystem);
 
 var express = require('express');
-var app = express();
- app.get('/', function (req, res) {
-    // ruleid:generic_path_traversal
-   // var filePath = path.join(__dirname, '/' + req.query.load);
-   // var readStream = fileSystem.createReadStream(filePath);
-    // ruleid:generic_path_traversal
-    fileSystem.readFile(req.query.foo);
-    // ruleid:generic_path_traversal
-    console.log(fileSystem.readFileSync(req.query.nar, 'utf8'));
-    // ruleid:generic_path_traversal
-    var foo = req.query.y;
-    fileSystem.readFile(foo);
-    fileSystem.readFile(foo + "bar");
-    readStream.pipe(res);
-}); 
+
 
 app.get('/foo', function (req, res) {
     // ruleid:generic_path_traversal
