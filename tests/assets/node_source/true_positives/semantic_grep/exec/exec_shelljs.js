a50f9de2107b56a22ab93251f1e393d6b23ca23f
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-23 20:10:35 +0530

    Update exec_shelljs.js

--- tests/assets/node_source/true_positives/semantic_grep/exec/exec_shelljs.js
+++ tests/assets/node_source/true_positives/semantic_grep/exec/exec_shelljs.js
@@ -2,10 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/greeting', (req, res) => {
-    // ruleid:shelljs_os_command_exec
-    return shell.exec(req.query, { silent: true })
-})
+
 
 
 */
