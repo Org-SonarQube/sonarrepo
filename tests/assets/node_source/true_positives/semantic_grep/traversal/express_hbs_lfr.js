f504814e8964a756375cd354ba0ac523a39f063a
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-23 17:45:10 +0530

    Update express_hbs_lfr.js

--- tests/assets/node_source/true_positives/semantic_grep/traversal/express_hbs_lfr.js
+++ tests/assets/node_source/true_positives/semantic_grep/traversal/express_hbs_lfr.js
@@ -2,9 +2,7 @@
 /* var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-     res.render('index')
-});
+
 
 router.post('/', function(req, res, next) {
     // ruleid:express_lfr_warning
