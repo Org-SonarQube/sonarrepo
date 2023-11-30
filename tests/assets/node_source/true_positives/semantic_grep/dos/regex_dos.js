4a5fe11162a35991fa95a3c6d8a91f7a6baa6e76
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-30 22:23:40 +0530

    Update regex_dos.js

--- tests/assets/node_source/true_positives/semantic_grep/dos/regex_dos.js
+++ tests/assets/node_source/true_positives/semantic_grep/dos/regex_dos.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const router = express.Router()
 
 
@@ -53,4 +53,4 @@ http.createServer(function (request, response) {
 
 }).listen(8888);
 
-
+*/
